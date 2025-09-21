0322c475c50f1ef4bcd36eb82c420a0e0df04e1e
Oscar Iván Magallón Franco <95940134+magfradeveloper@users.noreply.github.com>
2025-09-20 23:51:07 -0600

    Cambio de Url

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,20 +1,20 @@
-import axios from 'axios'
+import axios from "axios";
 
 // We create our own axios instance and set a custom base URL.
 // Note that if we wouldn't set any config here we do not need
 // a named export, as we could just `import axios from 'axios'`
 const api = axios.create({
-  // baseURL:'https://localhost:44312/api/'
-  baseURL: 'https://api.gestimed.com.mx/api/'
-})
+  baseURL: "https://localhost:44312/api/",
+  // baseURL: 'https://api.gestimed.com.mx/api/'
+});
 
 // for use inside Vue files through this.$axios and this.$api
 // (only in Vue Options API form)
 export default ({ app }) => {
-  app.config.globalProperties.$axios = axios
-  app.config.globalProperties.$api = api
-}
+  app.config.globalProperties.$axios = axios;
+  app.config.globalProperties.$api = api;
+};
 
 // Here we define a named export
 // that we can later use inside .js files:
-export { axios, api }
+export { axios, api };
