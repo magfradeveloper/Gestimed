529ab7bd22dd88171e8c5b76be226e621cee79ac
Oscar Iván Magallón Franco <95940134+magfradeveloper@users.noreply.github.com>
2026-09-20 19:27:15 -0600

    Cambios de ruta

--- src/boot/rutas.js
+++ src/boot/rutas.js
@@ -1,16 +1,15 @@
 // We create our own axios instance and set a custom base URL.
 // Note that if we wouldn't set any config here we do not need
 // a named export, as we could just `import axios from 'axios'`
-const ruta = 'https://api.gestimed.com.mx/Files'
+const ruta = "https://apigestimed.compraloahora.com.mx/Files";
 // const ruta = 'https://localhost:44312//Files'
-
 
 // for use inside Vue files through this.$axios and this.$api
 // (only in Vue Options API form)
 export default ({ app }) => {
-  app.config.globalProperties.$ruta = ruta
-}
+  app.config.globalProperties.$ruta = ruta;
+};
 
 // Here we define a named export
 // that we can later use inside .js files:
-export { ruta }
+export { ruta };
